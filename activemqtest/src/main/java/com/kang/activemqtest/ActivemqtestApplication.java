f2fa50dc4eeceeeea88cea48caff9b33c732bc46
kl <2562811578@qq.com>
2020-05-25 01:00:28 +0800

    mq init code

---
+++ activemqtest/src/main/java/com/kang/activemqtest/ActivemqtestApplication.java
@@ -0,0 +1,15 @@
+package com.kang.activemqtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
+
+@SpringBootApplication
+@EnableJms
+public class ActivemqtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivemqtestApplication.class, args);
+    }
+
+}
---
+++ activemqtest/src/test/java/com/kang/activemqtest/ActivemqtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kang.activemqtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivemqtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
